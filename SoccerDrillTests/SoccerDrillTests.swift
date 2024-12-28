6dbbecaf6c541ae409b02a619c125668410f6f22
Gareth Murphy <16783958+garethcmurphy@users.noreply.github.com>
2024-12-28 14:30:11 +0100

    Initial Commit

---
+++ SoccerDrillTests/SoccerDrillTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerDrillTests.swift
+//  SoccerDrillTests
+//
+//  Created by Gareth Murphy on 28/12/2024.
+//
+
+import Testing
+@testable import SoccerDrill
+
+struct SoccerDrillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
