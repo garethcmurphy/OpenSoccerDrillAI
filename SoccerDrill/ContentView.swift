d487b96960bde8b8e5bb63d3453d0d01d3e95a30
Gareth Murphy <16783958+garethcmurphy@users.noreply.github.com>
2025-01-04 09:01:04 +0100

    add time and energy

--- SoccerDrill/ContentView.swift
+++ SoccerDrill/ContentView.swift
@@ -6,6 +6,8 @@ struct Drill: Identifiable {
     let description: String
     let details: String
     let image: String
+    let calories: Int
+    let time: Int
 }
 
 // MARK: - Drill Detail View
@@ -20,6 +22,8 @@ struct DrillDetailView: View {
             Text(drill.description)
                 .font(.body)
                 .padding()
+            Text("Calories: \(drill.calories) calories")
+            Text("Time: \(drill.time) minutes")
             Text(drill.details)
                 .font(.body)
                 .padding()
@@ -40,21 +44,31 @@ struct ContentView: View {
     let drills = [
         Drill(name: "Passing Drill", description: "Improve your short and long passes with this drill."
              , details: "Passing is a fundamental skill in soccer. \nThis drill will help you improve your short and long passes. \nFocus on accuracy and power. \nUse both feet to pass the ball. \nKeep your body over the ball and follow through with your kicking foot.",
-              image: "zigzag_layout"
+              image: "zigzag_layout",
+              calories: 100,
+                time: 10
              ),
         Drill(name: "Shooting Drill", description: "Enhance accuracy and power with focused shooting practice.",
              details: "Shooting is a key skill for forwards and midfielders. This drill will help you improve your accuracy and power. Focus on technique and placement. Use both feet to shoot",
-              image: "hollow_square_layout"
+              image: "hollow_square_layout",
+              calories: 100,
+                time: 10
              ),
         Drill(name: "Dribbling Drill", description: "Work on close ball control and quick direction changes.",
              details: "Dribbling is a key skill for attacking players. This drill will help you improve your close ball control and quick direction changes. Focus on keeping the ball close to your feet and using both feet to dribble. Use cones to practice dribbling around obstacles.",
-              image: "diagonal_layout"),
+              image: "diagonal_layout",
+              calories: 100,
+                time: 10),
         Drill(name: "Defensive Drill", description: "Learn positioning and tackle timing.",
               details:  "Defending is a key skill for defenders and midfielders. This drill will help you improve your positioning and tackle timing. Focus on staying goal-side of the attacker and jockeying to force them wide. Time your tackles carefully and use your body to block shots.",
-              image: "row_layout"),
+              image: "row_layout",
+              calories: 100,
+                time: 10),
         Drill(name: "Goalkeeping Drill", description: "Focus on saves, positioning, and distribution.",
               details:  "Goalkeeping is a specialized position that requires unique skills. This drill will help you improve your saves, positioning, and distribution. Focus on getting your body behind the ball and using your hands to make saves. Position yourself well to cut down angles and make it difficult for attackers to score. Work on distributing the ball quickly and accurately to start counterattacks.",
-              image: "zizag_layout"
+              image: "zigzag_layout",
+              calories: 100,
+                time: 10
              )
     ]
     
@@ -152,11 +166,18 @@ struct PlayersView: View {
             Text("Layouts")
                 .font(.largeTitle)
                 .padding()
-            Spacer()
-            Image("image")
+   
+
+            Image("zigzag_layout")
                 .resizable() // Makes the image resizable
-                .scaledToFit() // Scales it to fit within the frame
-                .frame(width: 200, height: 200)
+                .scaledToFit()
+            Image("column_layout")
+                .resizable() // Makes the image resizable
+                .scaledToFit()
+            Image("row_layout")
+                .resizable() // Makes the image resizable
+                .scaledToFit()// Scales it to fit within the frame
+                
         }
         .navigationTitle("Layouts")
     }
